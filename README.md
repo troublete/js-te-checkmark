5650c65ae29805988e9a2e301cf9a751c7b347d2
Willi Eßer <willi.esser@troublete.com>
2017-11-13 21:32:37 +0100

    (pkg) init commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# te-checkmark
+
+> Checkmark button custom element
+
+[![Build Status](https://travis-ci.org/troublete/te-checkmark.svg?branch=master)](https://travis-ci.org/troublete/te-checkmark)
+
+## Purpose
+
+A checkbox alternative with no value, but caption and simple design. 
+
+## Install
+
+```sh
+npm i -S troublete/te-checkmark
+```
+
+## Usage
+
+```html
+<te-checkmark>Optional</checkmark>
+```
+
+## Properties
+
+| Property | Type | Description |
+|---|---|---|
+| checked | `boolean` | An indicator if the option button is checked. |
+
+## CSS Variables
+
+| Var | Description
+|---|---|
+| --te-checkmark-padding | The padding arround the checkmark (and the optional caption). |
+| --te-checkmark-checked-color | The `:checked` state color for font and border. |
+
+## License
+
+GPL-2.0 © Willi Eßer
---
+++ te-checkmark.js
@@ -0,0 +1,69 @@
+class TeCheckmark extends HTMLElement {
+	constructor() {
+		super();
+		this.addEventListener('click', () => {
+			if (this.checked) {
+				this.checked = false;
+			} else {
+				this.checked = true;
+			}
+		});
+	}
+
+	set checked(checked) {
+		if (checked) {
+			this.setAttribute('checked', '');
+		} else {
+			this.removeAttribute('checked');
+		}
+	}
+
+	get checked() {
+		return this.hasAttribute('checked');
+	}
+
+	static get observedAttributes() {
+		return ['checked'];
+	}
+
+	connectedCallback() {
+		const shadowDom = this.attachShadow({mode: 'open'});
+		shadowDom.appendChild(this.template);
+	}
+
+	get template() {
+		const template = document.createElement('template');
+		template.innerHTML = `
+		<style>
+		:host {
+			display:inline-block;
+			font-family:-apple-system,BlinkMacSystemFont,segoe ui,Roboto,Helvetica,Arial,sans-serif,apple color emoji,segoe ui emoji,segoe ui symbol;
+			font-size:1rem;
+			line-height:1rem;
+			height:1rem;
+			padding:var(--te-checkmark-padding, .35rem);
+			min-width:1rem;
+			border:1px solid black;
+			text-align:center;
+			border-radius:calc(2rem + var(--te-checkmark-padding, .35rem));
+			font-weight:lighter;
+			cursor:pointer;
+			user-select:none;
+		}
+		:host([checked]) {
+			color:var(--te-checkmark-checked-color, #4CC552);
+			border-color:var(--te-checkmark-checked-color, #4CC552);
+		}
+		</style>
+		<slot></slot>
+		<span>✓</span>`;
+
+		return template.content.cloneNode(true);
+	}
+}
+
+if (window.customElements) {
+	window.customElements.define('te-checkmark', TeCheckmark);
+} else {
+	throw new Error(`te-checkmark could not be registered`);
+}
